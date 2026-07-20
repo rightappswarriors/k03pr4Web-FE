27a347808f455a76bfdbcd582467c19ad74cb181
Rusiana Emmanuel <rai.rusianaemmanuel@gmail.com>
2026-07-20 19:10:36 +0800

    tryfix

--- app/wholesale/products/page.tsx
+++ app/wholesale/products/page.tsx
@@ -1,3 +1,4 @@
+//k03pr4Web-FE\app\wholesale\products\page.tsx
 export const dynamic = "force-dynamic";
 import Header from "@/components/layout/Header";
 import WholesaleProductCard from "@/components/wholesale/WholesaleProductCard";
